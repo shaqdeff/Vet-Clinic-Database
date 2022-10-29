f91fa38c201c957d256dc1a1841d00942e6f8b48
shaqdeff <shaquillendunda@gmail.com>
2022-10-29 19:41:26 +0300

    Insert data into owners and species tables

--- data.sql
+++ data.sql
@@ -12,3 +12,14 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', date '1998-10-13', 3 , TRUE, 17);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', date '2022-05-14', 4 , TRUE, 22);
 
+INSERT INTO owners (full_name, age) VALUES
+  ('Sam Smith', 34),
+  ('Jennifer Orwell', 19),
+  ('Bob', 45),
+  ('Melody Pond', 77),
+  ('Dean Winchester', 14),
+  ('Jodie Whittaker', 38);
+
+  INSERT INTO species (name) VALUES 
+  ('Pokemon'),
+  ('Digimon');
