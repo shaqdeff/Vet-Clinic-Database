7ef4a927773d4925c59b48a7c479d6e83e83d456
shaqdeff <shaquillendunda@gmail.com>
2022-10-29 21:08:42 +0300

    Rectify modifications

--- data.sql
+++ data.sql
@@ -1,16 +1,17 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', date '2020-02-03', 0 , TRUE, 10.23);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', date '2018-11-15', 2 , TRUE, 8.0);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', date '2021-01-07', 1 , FALSE, 15.04);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', date '2017-05-12', 2 , TRUE, 11);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', date '2020-02-08', 0 , FALSE, -11);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', date '2021-11-15', 2 , TRUE, -5.7);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtle', date '1993-04-02', 3 , FALSE, -12.13);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', date '2005-06-12', 1 , TRUE, -45);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon', date '2005-06-07', 7 , TRUE, 20.4);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom', date '1998-10-13', 3 , TRUE, 17);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Ditto', date '2022-05-14', 4 , TRUE, 22);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES 
+ ('Agumon', date '2020-02-03', 0 , TRUE, 10.23),
+ ('Gabumon', date '2018-11-15', 2 , TRUE, 8.0),
+ ('Pikachu', date '2021-01-07', 1 , FALSE, 15.04),
+ ('Devimon', date '2017-05-12', 2 , TRUE, 11),
+ ('Charmander', date '2020-02-08', 0 , FALSE, -11),
+ ('Plantmon', date '2021-11-15', 2 , TRUE, -5.7),
+ ('Squirtle', date '1993-04-02', 3 , FALSE, -12.13),
+ ('Angemon', date '2005-06-12', 1 , TRUE, -45),
+ ('Boarmon', date '2005-06-07', 7 , TRUE, 20.4),
+ ('Blossom', date '1998-10-13', 3 , TRUE, 17),
+ ('Ditto', date '2022-05-14', 4 , TRUE, 22);
 
 INSERT INTO owners (full_name, age) VALUES
   ('Sam Smith', 34),
